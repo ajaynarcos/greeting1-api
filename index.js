e3f5be10bd3b38313ea385616a8ae6d69e71a20d
ajay <ajaynarcos@gmail.com>
2022-10-13 17:57:19 +0530

    .

---
+++ index.js
@@ -0,0 +1,21 @@
+//importing packages
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const greet = require('./routes/greet');
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+// adding routes
+app.use('/greet', greet);
+
+app.get('/',(req,res) =>{
+    res.json("welcome to the api")
+
+});
+
+//port
+const port = process.env.PORT || 5500;
+app.listen(port, () => console.log(`Listening on Port: ${port}`));
